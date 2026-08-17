3d1977179594a51c0d838764efc2d3f7a764c29e
Qing-Novel <136884931+Qing-Novel@users.noreply.github.com>
2026-08-17 18:45:02 +0800

    Update URL for shonen_jump_plus.js

--- shonen_jump_plus.js
+++ shonen_jump_plus.js
@@ -4,7 +4,7 @@ class ShonenJumpPlus extends ComicSource {
   version = "1.1.2"; // 升级源版本号
   minAppVersion = "1.2.1";
   url =
-    "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/shonen_jump_plus.js";
+    "https://cdn.jsdelivr.net/gh/Qing-Novel/venerax-configs@main/index.json";
 
   deviceId = this.generateDeviceId();
   bearerToken = null;
